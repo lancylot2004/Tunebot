1af5a070e5d5c8370dac65ea9df1d198b1ecdb1b
Hongming (Lancelot) Liu <lancylot2004@gmail.com>
2022-07-01 18:42:10 +0100

    Initial Commit

---
+++ Tunebot/TunebotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunebotApp.swift
+//  Tunebot
+//
+//  Created by lancylot2004 on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TunebotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
